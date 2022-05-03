66f324042f68c9e8443382f731c8fe695fda3fed
Richard Zahn <richard-zahn@web.de>
2022-05-03 07:50:42 +0200

    add basic bar chart for death cases

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
-def func():
-    dataframe = pd.read_csv('data/death_cases/Monthly_Deaths_Per_Age_Group_Women_2016-2022.csv')
-    print(dataframe)
+def load_monthly_female_death_cases_per_age_group():
+    return pd.read_csv('data/death_cases/Monthly_Deaths_Per_Age_Group_Women_2016-2022.csv')
+
+
+def load_monthly_male_death_cases_per_age_group():
+    return pd.read_csv('data/death_cases/Monthly_Deaths_Per_Age_Group_Men_2016-2022.csv')
+
+
+def load_population_development():
+    return pd.read_csv('data/population_development.csv', delimiter=';')
+
+
+def load_age_pyramid():
+    return pd.read_csv('data/age_pyramid.csv', delimiter=';')
+
+
+def main():
+    population_development = load_population_development()
+    deaths = population_development[['Jahr', 'Sterbefälle']]
+    deaths.plot.bar(x='Jahr', y='Sterbefälle', rot=0)
+
+    plt.show()
 
 
 if __name__ == '__main__':
-    func()
+    main()
 
