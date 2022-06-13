dbaa79f9730fdc59d24cf8a37f51800bc13712e1
Richard <Richard-Zahn@web.de>
2022-06-13 11:30:45 +0200

    fix dates

--- Lockdown.py
+++ Lockdown.py
@@ -1,29 +1,29 @@
 events = {
-    '01/06/20': 'Phased Reopening of schools',
-    '15/06/20': 'Non-essential shops reopen',
-    '14/08/20': 'Indoor Activities reopened',
-    '14/09/20': 'Indoor & Outdoor Gatherings with max 6 people',
-    '22/09/20': 'Work from Home & 10PM curfew',
-    '14/10/20': 'New 3-Tier system of covid restrictions',
-    '08/03/21': 'Schools reopen',
-    '29/03/21': '6 Persons or 2 households allowed',
-    '12/04/21': 'Non-essential shops, gyms, zoos reopen',
-    '17/05/21': 'Outdoor with 30 people. 10.000 Spectators for very large events',
-    '19/07/21': 'Most legal limits on social contact are removed',
-    '10/12/21': 'Face masks become compulsory in most public indoor venues'
+    '2020-06-01': 'Phased Reopening of schools',
+    '2020-06-15': 'Non-essential shops reopen',
+    '2020-08-14': 'Indoor Activities reopened',
+    '2020-09-14': 'Indoor & Outdoor Gatherings with max 6 people',
+    '2020-09-22': 'Work from Home & 10PM curfew',
+    '2020-10-14': 'New 3-Tier system of covid restrictions',
+    '2021-03-08': 'Schools reopen',
+    '2021-03-29': '6 Persons or 2 households allowed',
+    '2021-04-12': 'Non-essential shops, gyms, zoos reopen',
+    '2021-05-17': 'Outdoor with 30 people. 10.000 Spectators for very large events',
+    '2021-07-19': 'Most legal limits on social contact are removed',
+    '2021-12-10': 'Face masks become compulsory in most public indoor venues'
 }
 
 lockdowns = {
     1: {
-        'start_date': '26/03/20',
-        'end_date': '11/05/20'
+        'start_date': '2020-03-26',
+        'end_date': '2020-05-11'
     },
     2: {
-        'start_date': '05/11/20',
-        'end_date': '02/12/20'
+        'start_date': '2020-11-05',
+        'end_date': '2020-12-02'
     },
     3: {
-        'start_date': '06/01/21',
-        'end_date': '19/07/21'
+        'start_date': '2021-01-06',
+        'end_date': '2021-07-19'
     }
 }
