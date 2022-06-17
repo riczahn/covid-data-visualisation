71a0fcdd264f968e354742596d9d4facbfc17462
Richard <Richard-Zahn@web.de>
2022-06-17 10:52:21 +0200

    add diagram for all deaths in 2020 and 2021

--- FigureProvider.py
+++ FigureProvider.py
@@ -1,3 +1,5 @@
+import datetime
+
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -7,6 +9,37 @@ from Formatter import Formatter
 class FigureProvider:
     def __init__(self):
         self.formatter = Formatter()
+
+    def get_uk_overall_deaths(self, figure_size):
+        df = pd.read_csv('data/uk/deaths_registered_summary_statistics.csv').iloc[:, :-1]
+        df = df.set_index('Week number').T
+
+        data_2021 = {}
+        data_2020 = {}
+
+        # transform data into a more usable format, where the week number is transformed
+        # to the date of that weeks monday
+        for index, row in df.iterrows():
+            week_as_date_2021 = datetime.datetime.strptime('2021-W' + index.strip() + '-1', "%Y-W%W-%w").strftime(
+                "%Y-%m-%d")
+            data_2021[week_as_date_2021] = row['Total deaths, all ages (2021)']
+
+            week_as_date_2020 = datetime.datetime.strptime('2020-W' + index.strip() + '-1', "%Y-W%W-%w").strftime(
+                "%Y-%m-%d")
+            data_2020[week_as_date_2020] = row['Total deaths, all ages (2020)']
+
+        data_of_both_years = {**data_2020, **data_2021}
+        cool_df = pd.DataFrame.from_dict(data_of_both_years, orient='index', columns=['Weekly Deaths'])
+
+        # parse values to float
+        cool_df = cool_df.apply(lambda x: x.str.replace(',', '').astype(float), axis=1)
+
+        figure = plt.Figure(figsize=figure_size, dpi=100)
+        axis = figure.add_subplot(111)
+        cool_df.plot(use_index=True, ax=axis, kind='bar')
+        # todo maybe also plot the part of covid deaths? So that we can see the impact of covid? Or in a different graph
+
+        return figure
 
     def get_uk_covid_tests_and_cases(self, figure_size):
         data = pd.read_csv('data/uk/UK_new_cases_and_tests.csv', parse_dates=['date'])
@@ -87,6 +120,7 @@ class FigureProvider:
     def get_all_figures(self, figure_size):
         return {
             'Tests and Cases': self.get_uk_covid_tests_and_cases(figure_size),
+            'Overall Deaths 2020-2021': self.get_uk_overall_deaths(figure_size),
             'Daily Deaths': self.get_uk_daily_deaths(figure_size),
             'Weekly Deaths by Age Group': self.get_uk_deaths_by_week(figure_size),
             'Pre-Conditions': self.get_uk_pre_conditions_for_covid_deaths(figure_size),
